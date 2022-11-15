4b49649b3caa9f3725b9ce2416fb458e5b4a0569
beccajekogian <80211444+beccajekogian@users.noreply.github.com>
2022-11-15 09:29:38 -0500

    Add files via upload

---
+++ Renewable Energy Project/data/generateJSON.js
@@ -0,0 +1,41 @@
+// "Google Drive"/"My Drive"/"Upper School"/"12 - Senior"/"Adv Topics Comp Sci"/?Renewable Energy Project"
+const fs = require('fs');
+
+let finalSet = {};
+
+let rawdataset = fs.readFileSync('dataset.csv', 'utf8');
+
+let dataSet = rawdataset.split("\n");
+
+dataSet.forEach(function(data) {
+  let info = data.split(',');
+
+  let country = info[0].trim();
+
+  if(country!="Entity"){
+    let countryStats = {};
+    let year = info[2].trim();
+    let energy = info[3].trim();
+
+    //country[data[0].trim()] = countryStats;
+    if (!finalSet[country]) {
+    finalSet[country] = {};
+  }
+
+    finalSet[country][year] = energy;
+  }
+});
+
+
+fs.writeFileSync('countries.json', JSON.stringify(finalSet), 'utf8');
+
+
+/*
+Africa:
+  1970 = 10;
+  1971 = 11;
+
+
+
+
+*/
