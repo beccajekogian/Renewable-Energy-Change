c1fc2faefab77b7ca1146634d711a9cb6b32425b
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2022-11-21 20:42:49 -0500

    new

--- Renewable Energy Project/src/prebuild.js
+++ Renewable Energy Project/src/prebuild.js
@@ -1,5 +1,6 @@
 
 const fs = require('fs');
+const path = require('path');
 
 
 // fs.copyFileSync(source, destination)
@@ -7,24 +8,62 @@ const fs = require('fs');
 // fs.mkdirSync('public');
 
 
-const dir = 'public'
+const directory = '../public/'
 
-fs.unlinkSync('../public');
 
-// delete directory recursively
-fs.rmdir(dir, { recursive: true }, err => {
-  if (err) {
-    throw err
+if (fs.existsSync('public')){
+fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (let file of files) {
+    fs.unlink(directory + file, (err) => {
+      if (err) throw err;
+    });
   }
+});
+} else {
+  console.log("hi")
+}
 
-  console.log(`${dir} is deleted!`)
-})
-
-
-fs.mkdir(dir, { recursive: true }, err => {
-  if (err) {
-    throw err
-  }
-
-  console.log(`${dir} is added!`)
-})
+// //
+// // if (fs.existsSync('public')){
+// //
+// //   fs.rmdir('Renewable Energy Project/public', () => {
+// //   console.log("Folder Deleted!");
+// //   });
+// //
+// // } else{
+// //
+// //
+// //   const fs = require('fs');
+// //   // const path = require('path');
+// //
+// //   fs.mkdir(path.join(__dirname, 'test'), (err) => {
+// //       if (err) {
+// //           return console.error(err);
+// //       }
+// //       console.log('Directory created successfully!');
+// //   });
+//
+//   //
+//   // fs.mkdir(dir, { recursive: true }, err => {
+//   //   if (err) {
+//   //     throw err
+//   //   }
+//   //
+//   //   console.log(`${dir} is added!`)
+//   // })
+//
+//   // fs.mkdirSync('../public');
+//   fs.rename('src/styles.css', 'public/styles.css')
+//   // function (err) {
+//   //   //if (err) throw err
+//   //   console.log('Successfully renamed - AKA moved!')
+//   // });
+//
+// }
+//
+// ///fs.unlinkSync('../public');
+//
+// // delete directory recursively
+//
