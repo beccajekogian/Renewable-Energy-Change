ccf3ca218b466f5dee23994d7a9ffa90d712cd9d
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2022-11-21 21:12:00 -0500

    Revert "new"
    This reverts commit c1fc2faefab77b7ca1146634d711a9cb6b32425b.

--- Renewable Energy Project/src/prebuild.js
+++ Renewable Energy Project/src/prebuild.js
@@ -1,6 +1,5 @@
 
 const fs = require('fs');
-const path = require('path');
 
 
 // fs.copyFileSync(source, destination)
@@ -8,62 +7,24 @@ const path = require('path');
 // fs.mkdirSync('public');
 
 
-const directory = '../public/'
+const dir = 'public'
 
+fs.unlinkSync('../public');
 
-if (fs.existsSync('public')){
-fs.readdir(directory, (err, files) => {
-  if (err) throw err;
-
-  for (let file of files) {
-    fs.unlink(directory + file, (err) => {
-      if (err) throw err;
-    });
+// delete directory recursively
+fs.rmdir(dir, { recursive: true }, err => {
+  if (err) {
+    throw err
   }
-});
-} else {
-  console.log("hi")
-}
 
-// //
-// // if (fs.existsSync('public')){
-// //
-// //   fs.rmdir('Renewable Energy Project/public', () => {
-// //   console.log("Folder Deleted!");
-// //   });
-// //
-// // } else{
-// //
-// //
-// //   const fs = require('fs');
-// //   // const path = require('path');
-// //
-// //   fs.mkdir(path.join(__dirname, 'test'), (err) => {
-// //       if (err) {
-// //           return console.error(err);
-// //       }
-// //       console.log('Directory created successfully!');
-// //   });
-//
-//   //
-//   // fs.mkdir(dir, { recursive: true }, err => {
-//   //   if (err) {
-//   //     throw err
-//   //   }
-//   //
-//   //   console.log(`${dir} is added!`)
-//   // })
-//
-//   // fs.mkdirSync('../public');
-//   fs.rename('src/styles.css', 'public/styles.css')
-//   // function (err) {
-//   //   //if (err) throw err
-//   //   console.log('Successfully renamed - AKA moved!')
-//   // });
-//
-// }
-//
-// ///fs.unlinkSync('../public');
-//
-// // delete directory recursively
-//
+  console.log(`${dir} is deleted!`)
+})
+
+
+fs.mkdir(dir, { recursive: true }, err => {
+  if (err) {
+    throw err
+  }
+
+  console.log(`${dir} is added!`)
+})
