d8071df5d737e19c41bb4ae89a07faa1e744648f
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2022-11-28 20:41:00 -0500

    new and works maybe

--- Renewable Energy Project/src/prebuild.js
+++ Renewable Energy Project/src/prebuild.js
@@ -1,30 +1,44 @@
 
 const fs = require('fs');
+const path = require('path');
 
 
 // fs.copyFileSync(source, destination)
 // fs.rmdirSync('../public');
 // fs.mkdirSync('public');
 
-
-const dir = 'public'
-
-fs.unlinkSync('../public');
-
-// delete directory recursively
-fs.rmdir(dir, { recursive: true }, err => {
-  if (err) {
-    throw err
-  }
-
-  console.log(`${dir} is deleted!`)
-})
+let current_paths = "../src/";
+let new_path = "../public/";
+let style = 'styles.css';
+//let headers = styles.css;
 
 
-fs.mkdir(dir, { recursive: true }, err => {
-  if (err) {
-    throw err
-  }
+if (fs.existsSync('Renewable Energy Project/public/styles.css')){
 
-  console.log(`${dir} is added!`)
-})
+  fs.rename('Renewable Energy Project/public/' + style, 'Renewable Energy Project/src/' + style, (err) => {
+      if (err) throw err;
+  });
+
+  fs.readdir('Renewable Energy Project/public', (err, files) => {
+    if (err) throw err;
+
+    for (let file of files) {
+      fs.unlink('Renewable Energy Project/public/' + file, (err) => {
+        if (err) throw err;
+      });
+    }
+    console.log(`${new_path} is deleted!`)
+
+  });
+
+} else if (fs.existsSync('Renewable Energy Project/src/styles.css')){
+
+        fs.rename('Renewable Energy Project/src/' + style, 'Renewable Energy Project/public/' + style, (err) => {
+            if (err) throw err;
+        });
+
+        fs.rename('Renewable Energy Project/src/headers.css', 'Renewable Energy Project/public/headers.css', (err) => {
+            if (err) throw err;
+        });
+
+}
