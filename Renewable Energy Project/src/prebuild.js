502abd7dcd59fab9007143a66ee75eb9c6f730d0
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2022-11-30 21:27:12 -0500

    new

--- Renewable Energy Project/src/prebuild.js
+++ Renewable Energy Project/src/prebuild.js
@@ -50,57 +50,76 @@ let style = 'styles.css';
         if (err) throw err;
     });
   }
-  else if (fs.existsSync('../public/styles.css')){
-    fs.rename('../public/styles.css', 'styles.css', (err) => {
+  if (fs.existsSync('about.html')){
+    fs.rename('about.html', '../public/about.html', (err) => {
         if (err) throw err;
     });
   }
+  // else if (fs.existsSync('../public/styles.css')){
+  //   fs.rename('../public/styles.css', 'styles.css', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
 
   if (fs.existsSync('headers.css')){
     fs.rename('headers.css', '../public/headers.css', (err) => {
         if (err) throw err;
     });
-  } else if (fs.existsSync('../public/headers.css')){
-    fs.rename('../public/headers.css', 'headers.css', (err) => {
-        if (err) throw err;
-    });
   }
+  // else if (fs.existsSync('../public/headers.css')){
+  //   fs.rename('../public/headers.css', 'headers.css', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
 
 
   if (fs.existsSync('images')){
     fs.rename('images', '../public/images', (err) => {
         if (err) throw err;
     });
-  } else if (fs.existsSync('../public/images')){
-    fs.rename('../public/images','images', (err) => {
-        if (err) throw err;
-    });
   }
-
-  if (fs.existsSync('about.html')){
-    fs.rename('about.html', '../public/about.html', (err) => {
-        if (err) throw err;
-    });
-  } else if (fs.existsSync('../public/about.html')){
-    fs.rename('../public/about.html', 'about.html', (err) => {
-        if (err) throw err;
-    });
-  }
+  // else if (fs.existsSync('../public/images')){
+  //   fs.rename('../public/images','images', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
 
 
-if (fs.existsSync('../public/Spain.html') && !fs.existsSync('../public/about.html') && !fs.existsSync('../public/images') && !fs.existsSync('../public/headers.css')){
+  // else if (fs.existsSync('../public/about.html')){
+  //   fs.rename('../public/about.html', 'about.html', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
+
+
+if (fs.existsSync('../public/Spain.html')){
+ // && !fs.existsSync('../public/about.html') && !fs.existsSync('../public/images') && !fs.existsSync('../public/headers.css')){
+
+  // if (fs.existsSync('../public/images')){
+  //   fs.rename('../public/images','images', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
+
   fs.readdir('../public', (err, files) => {
     if (err) throw err;
 
     console.log(__dirname);
 
     for (let file of files) {
-      if (file != '../public/about.html' && file != '../public/headers.css'){
-      fs.unlink('../public/' + file, (err) => {
-        if (err) throw err;
-      });
+      console.log(file);
+      if (file != 'about.html' && file != 'headers.css' && file != 'styles.css' && file!= 'images'){
+        fs.unlink('../public/' + file, (err) => {
+          if (err) throw err;
+        });
+      }
     }
-  }
     console.log(`public stuff is deleted!`)
   });
+
+  // if (fs.existsSync('images')){
+  //   fs.rename('images', '../public/images', (err) => {
+  //       if (err) throw err;
+  //   });
+  // }
 }
